d6fa8f65ffc6526e7496cf21abb33684726e9b65
A small Web crawler <1033480555@qq.com>
2016-07-09 19:55:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import unittest
 from main import is_prime
+from main import print_next_prime
 
 class PrimesTestCase(unittest.TestCase):
     """Tests for `primes.py`."""
@@ -7,6 +8,7 @@ class PrimesTestCase(unittest.TestCase):
     def test_is_five_prime(self):
         """Is five successfully determined to be prime?"""
         self.assertFalse(is_prime(5))
+    def test_next_prim(self):
         self.assertTrue(print_next_prime(5))
 
 if __name__ == '__main__':
